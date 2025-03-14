7ae744c8a965f2f165ac761fbd405f0713c028fc
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-03-15 03:31:49 +0700

    update vercek speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,5 +7,6 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 );

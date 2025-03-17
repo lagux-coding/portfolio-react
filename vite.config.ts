e09669c667cc3bbc3ca1e6fb659b7918eedea915
Nguyen Hien Trung Nam <nguyenhientrungnam@gmail.com>
2025-03-17 09:17:26 +0700

    optimized assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import compression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), compression()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
-    allowedHosts: ["contents-population-moreover-spent.trycloudflare.com"],
+    cors: true,
   },
 });
